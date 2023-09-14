0d440d20c3ad153dfa1c6b6a5d9bc7df63d5d26c
Davstx <davidsilvateixeira5@hotmail.com>
2023-09-14 16:32:05 -0300

    Commit do exercício 1 da aula 2 resolvido e pasta criada para os arquivos da aula 3

--- ExerciciosAula02/src/Aula2Exercicio1.java
+++ ExerciciosAula02/src/Aula2Exercicio1.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
 public class Aula2Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int numero2 = scanner.nextInt();
+
+        scanner.close();
+        String numerosPrimos = "";
+
+        for (int i = 2; i <= numero2; i++){
+            int contadorResto0 = 0;
+            for (int divisor = 1; divisor <= i; divisor++) {
+                if (i % divisor == 0) {
+                    contadorResto0++;
+                }
+            }
+            if (contadorResto0 == 2 && i >= numero1) {
+                numerosPrimos += i + " ";
+            }
+        }
+        System.out.println("Os números primos no intervalo são: " + numerosPrimos);
+    }
 }
---
+++ exerciciosAula03/src/Aula3Exercicio1.java
@@ -0,0 +1,5 @@
+public class Aula3Exercicio1 {
+    public static void main(String[] args) {
+        
+    }
+}
