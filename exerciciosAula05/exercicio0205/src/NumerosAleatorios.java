def9fe6925290879e30e6e9484ae10623001e6a2
Davstx <davidsilvateixeira5@hotmail.com>
2023-10-02 14:01:51 -0300

    exercício 2 da aula 5 resolvido

--- exerciciosAula05/exercicio0205/src/NumerosAleatorios.java
+++ exerciciosAula05/exercicio0205/src/NumerosAleatorios.java
@@ -1,23 +1,38 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class NumerosAleatorios {
     public static void main(String[] args) {
         System.out.println("===== Jogo de Adivinhação =====");
         Random gerador = new Random();
         Scanner scanner = new Scanner(System.in);
-        int [] numeros = new int[10];
+        int[] numeros = new int[10];
+
         for (int i = 0; i < 10; i++) {
             numeros[i] = gerador.nextInt(101);
         }
-        System.out.println(java.util.Arrays.toString(numeros));
+
         System.out.print("Digite um número: ");
         int palpite = scanner.nextInt();
 
-        if (Arrays.asList(numeros).contains(palpite)) {
+        boolean numeroEstaNoArray = false;
+
+        for (int numero : numeros) {
+            if (numero == palpite) {
+                numeroEstaNoArray = true;
+                break;
+            }
+        }
+
+        if (numeroEstaNoArray) {
             System.out.println("O número está presente no array");
         } else {
             System.out.println("O número não está no Array");
         }
+        scanner.close();
     }
 }
+
+
+
+
